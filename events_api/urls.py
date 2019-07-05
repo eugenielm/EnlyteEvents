c0bf10cec4526e06a8c302c0ab9134ae23269759
eugenielm <eugenie.le.moulec@gmail.com>
2019-07-05 16:30:52 -0700

    Generate a yml file and add a schema view to the API

--- events_api/urls.py
+++ events_api/urls.py
@@ -1,9 +1,18 @@
 
 from . import views
-from django.urls import path
+from django.urls import re_path
+from rest_framework.schemas import get_schema_view
+from rest_framework.renderers import JSONOpenAPIRenderer
 
+
+# https://www.django-rest-framework.org/api-guide/schemas/#the-get_schema_view-shortcut
+schema_view = get_schema_view(
+    title="Events API",
+    renderer_classes=[JSONOpenAPIRenderer,]
+)
 
 urlpatterns = [
-    path('events/<int:pk>/', views.EventUpdateView.as_view()),
-    path('events/', views.EventsListView.as_view()),
+    re_path(r'^events/(?P<pk>\d+)/?$', views.EventUpdateView.as_view()),
+    re_path(r'^events/?$', views.EventsListView.as_view()),
+    re_path(r'^schema/?$', schema_view),
 ]
