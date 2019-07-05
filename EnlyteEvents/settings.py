ff02f258bb9c147d2465b97d35096c45b817eb6d
eugenielm <eugenie.le.moulec@gmail.com>
2019-07-05 16:47:18 -0700

    No need for paginated results

--- EnlyteEvents/settings.py
+++ EnlyteEvents/settings.py
@@ -136,8 +136,6 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50,
     'DEFAULT_RENDERER_CLASSES': (
         # render only json by default
         'rest_framework.renderers.JSONRenderer',
