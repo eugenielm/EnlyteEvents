9fa77295bb06500dce1bdbb75fc399459f097850
eugenielm <eugenie.le.moulec@gmail.com>
2019-07-05 17:26:10 -0700

    Request JSON data sent on event update: add extra validation for name and organization fields

--- events_api/serializers.py
+++ events_api/serializers.py
@@ -6,3 +6,22 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Event
         fields = ('name', 'organization', 'start_date', 'cost')
+
+    # https://www.django-rest-framework.org/api-guide/validators/#validation-in-rest-framework
+    # https://www.django-rest-framework.org/api-guide/serializers/#field-level-validation
+    def validate_name(self, value):
+        """
+        Check that the name of the event is at least 5 characters long.
+        """
+        if len(value) < 5:
+            raise serializers.ValidationError("The name of an event must contain at least 5 characters.")
+        return value
+
+    def validate_organization(self, value):
+        """
+        Check that the name of the organization is at least 3 characters long.
+        """
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                "The organization name of an event must contain at least 3 characters.")
+        return value
